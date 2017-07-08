ff3bc1a5b8c67abbfe833d071a94d395cefcf8bd
Brendan Doherty <brendan@doherty.net.nz>
2017-07-08 16:54:10 +1200

    inital version

---
+++ home.js
@@ -0,0 +1,28 @@
+domready(function() {
+	function matchHeight() {
+		jQuery(".main-bg > .row:nth-child(3) .col-md-4").height('');
+		var rows = {};
+		jQuery(".main-bg > .row:nth-child(3)").children().each(function(i, el) {
+			rows[Math.floor(jQuery(el).offset().top)] = [];
+		});
+		jQuery(".main-bg > .row:nth-child(3)").children().each(function(i, el) {
+			rows[Math.floor(jQuery(el).offset().top)].push(el);
+		});
+		for (row in rows) {
+			jQuery(rows[row]).height(jQuery(rows[row]).height()+'px');
+		}
+	}
+
+	function afterLoad() {
+		if(window.jQuery) {
+			jQuery(".featuredproperties .col-md-4").removeClass('col-md-4').addClass('col-md-6');	
+			jQuery(".main-bg > .row:nth-child(3) .col-md-2").addClass("col-sm-5 col-xs-5 col-md-4");
+			jQuery(window).resize(matchHeight);
+			setTimeout(matchHeight, 500);
+			matchHeight();
+		} else {
+			setTimeout(afterLoad,15);
+		}
+	}
+	setTimeout(afterLoad,150);
+});
