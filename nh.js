b7a5421a30c57b6555a938e89e079a133b71088e
Brendan Doherty <brendan@doherty.net.nz>
2017-07-21 22:55:59 +1200

    bq resize

--- nh.js
+++ nh.js
@@ -1,11 +1,11 @@
 domready(function() {
 	function matchHeight() {
-		jQuery(".main-bg > .row:nth-child(3) .col-md-4").height('');
+		jQuery(".main-bg > .row:nth-child(4) .col-md-4").height('');
 		var rows = {};
-		jQuery(".main-bg > .row:nth-child(3)").children().each(function(i, el) {
+		jQuery(".main-bg > .row:nth-child(4)").children().each(function(i, el) {
 			rows[Math.floor(jQuery(el).offset().top)] = [];
 		});
-		jQuery(".main-bg > .row:nth-child(3)").children().each(function(i, el) {
+		jQuery(".main-bg > .row:nth-child(4)").children().each(function(i, el) {
 			rows[Math.floor(jQuery(el).offset().top)].push(el);
 		});
 		for (row in rows) {
@@ -14,7 +14,7 @@ domready(function() {
 	}
 
 	jQuery(".featuredproperties .col-md-4").removeClass('col-md-4').addClass('col-md-6');	
-	jQuery(".main-bg > .row:nth-child(3) .col-md-2").addClass("col-sm-5 col-xs-5 col-md-4");
+	jQuery(".main-bg > .row:nth-child(4) .col-md-2").addClass("col-sm-5 col-xs-5 col-md-4");
 
 	var timer = null;
 	jQuery(window).on('resize visibilitychange', function() {
