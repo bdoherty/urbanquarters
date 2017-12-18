37c5ddae0fe502c8fff89216c0d1bfe51485ced5
Brendan Doherty <brendan@urbanquarters.co.nz>
2017-12-18 15:00:40 +1300

    enable disabled submit button

---
+++ hpterms.js
@@ -0,0 +1,9 @@
+jQuery('input[type=checkbox]').click(function() { 
+      enableButton(this.checked);
+})
+
+function enableButton(enable) {
+	jQuery('input.button.submit').attr('disabled', !enable)
+	jQuery('input.button.submit').css('background-color', enable ? '' : '#999');
+}
+enableButton(false);
